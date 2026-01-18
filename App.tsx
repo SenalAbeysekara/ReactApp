c2b332fb389375ee7689ab2be475ba71f998f9c3
SenalAbeysekara <senaldushyantha@gmail.com>
2026-01-18 17:07:24 +0530

    Day 1

--- App.tsx
+++ App.tsx
@@ -1,45 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { Text, View } from 'react-native';
+import CustomerLabel from './src/components/CustomerLabel';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <View style={{ alignItems: 'center'}}>  
+    <Text>Welcome</Text>
+    <CustomerLabel />
+  </View>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ src/components/CustomerLabel.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomerLabel = () => {
+    return (
+        <View>
+            <Text style={{
+                fontWeight: 'bold',
+                color: 'blue',
+                fontSize: 40
+            }}>CustomerLabel</Text>
+        </View>
+    )
+}
+
+export default CustomerLabel
