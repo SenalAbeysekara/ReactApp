ca020df6389efa5a9adbbc0236bf17cd0af820ea
SenalAbeysekara <senaldushyantha@gmail.com>
2026-02-08 17:40:28 +0530

    Day 11

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,17 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import React from 'react'
+import { Icon } from '@rneui/themed';
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View style={{
+      flex: 1,
+    }}>
+      <Header />
+    </View>
+  )
+}
+
+const Header = () => {
+  return (
+    <View style={{
+      flexDirection: 'row',
+      padding: '5%',
+      alignItems: 'center',
+    }}>
+
+      <Image source={require('../../assets/img/pizza1.png')} style={{ width: 40, height: 40 }} />
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Text style={{ fontSize: 30, fontFamily: 'Lobster-Regular' }}>Pizza Mania</Text>
+        <Text style={{ fontSize: 14, fontFamily: 'Poppins-Regular', color: 'gray', marginTop: -8 }}>Best Pizza In Anywhere</Text>
+      </View>
+
+      <View style={{
+        width: 40,
+        height: 40,
+      }}>
+        <Icon size={35} type='ionicon' name='notifications-outline'/>
+      </View>
+
     </View>
   )
 }
 
 export default HomePage
 
-const styles = StyleSheet.create({})
 
 
 
